608f0b676a3ce97fdb30b1d7cef0ccb80c839097
Denis Kloepfer <denis.kloepfer@concepts-and-training.de>
2019-07-15 14:30:01 +0200

    prg progress invalidation logic. expired progress will keep its internal status, but the lp-status may turn to failed if not other passed valid progress present
    make sure the assignemnt date will not jump on update
    
    removed some putative debug log code
    
    lang corrections
    
    rm debug-logging
    
    removed redundand code

--- Modules/StudyProgramme/test/ilStudyProgrammeUserProgressTest.php
+++ Modules/StudyProgramme/test/ilStudyProgrammeUserProgressTest.php
@@ -942,12 +942,13 @@ class ilStudyProgrammeUserProgressTest extends TestCase {
 		$this->assertTrue($progress1->isSuccessful());
 		$this->assertTrue($progress1->isSuccessfulExpired());
 		try {
-			$progress1->markFailed(6);
+			$progress1->invalidate();
 			$this->assertTrue(true);
 		} catch(ilException $e) {
 			$this->assertFalse('did throw');
 		}
-		$this->assertFalse($progress1->isSuccessful());
+		$this->assertTrue($progress1->isSuccessful());
+		$this->assertTrue($progress1->isInvalidated());
 	}
 
 
@@ -977,10 +978,11 @@ class ilStudyProgrammeUserProgressTest extends TestCase {
 		$this->assertTrue($progress1->isSuccessful());
 		$this->assertFalse($progress1->isSuccessfulExpired());
 		try {
-			$progress1->markFailed(6);
+			$progress1->invalidate();
 			$this->assertFalse('did not throw');
 		} catch(ilException $e) {
 			$this->assertTrue(true);
 		}
+		$this->assertFalse($progress1->isInvalidated());
 	}
 }
